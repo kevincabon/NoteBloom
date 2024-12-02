52c7305745666bc2d9738b85974752da67a8edbe
Kevin Cabon <45396141+kevincabon@users.noreply.github.com>
2024-12-02 09:25:03 +0100

    * chore(.gitignore): update .gitignore to exclude supabase/migrations directory * feat(supabase/client.ts): add support for environment variables VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY * chore(supabase/config.toml): remove supabase/config.toml file

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,10 +2,11 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const supabaseUrl = "https://ilbcouenlibklodworfr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsYmNvdWVubGlia2xvZHdvcmZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwMDYxMTQsImV4cCI6MjA0ODU4MjExNH0.pYuUBTVMUmBLrVQaZNnA_cLudw-z5xSmHPCXtt1zd6k";
+
+if (!import.meta.env.VITE_SUPABASE_URL) throw new Error('Missing env.VITE_SUPABASE_URL')
+if (!import.meta.env.VITE_SUPABASE_ANON_KEY) throw new Error('Missing env.VITE_SUPABASE_ANON_KEY')
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
